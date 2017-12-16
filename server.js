8e47c58f637cb0b778271f37cbe7911ff0f5e356
PrestonWinstead <prestonwinstead@me.com>
2017-12-15 18:23:25 -0600

    Create deployment port options

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var app = require('./server-config.js');
 require('dotenv').config();
 
-var port = process.env.PORT || 9000;
+if (process.env.NODE_ENV === 'development') {
+  var port = process.env.PORT || 9000;
+} else {
+  var port = process.env.PORT || 80;
+}
 
 app.listen(port);
 
